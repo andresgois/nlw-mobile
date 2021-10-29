1c7478f39956a5b1a57a0737ad576401fc0e68dd
André gois <andre.s.gois3@gmail.com>
2021-10-28 23:26:36 -0300

    styles

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,11 +1,9 @@
 import { StyleSheet } from 'react-native'
-
+import { COLORS } from '../../theme'
 
 export const styles = StyleSheet.create({
     container: {
         flex:1,
-        justifyContent: 'center',
-        alignContent: 'center',
-        color: 'red'
+        backgroundColor: COLORS.BLACK_SECONDARY,
     }
 });
