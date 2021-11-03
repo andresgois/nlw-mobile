58371e430960d1c3583ecd14421a2ea36dc7c57b
André gois <andre.s.gois3@gmail.com>
2021-11-02 23:24:35 -0300

    button component creation

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+import { FONTS } from '../../theme';
+
+
+export const styles = StyleSheet.create({
+  container: {
+  },
+  title: {
+    fontSize: 14,
+    fontFamily: FONTS.BOLD
+  },
+  button: {
+      height: 48,
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  icon: {
+      marginRight: 12
+  }
+});
