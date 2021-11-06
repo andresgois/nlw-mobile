d34581614e7091da1d84945704f28edc435e4c23
André gois <andre.s.gois3@gmail.com>
2021-11-05 23:59:46 -0300

    creating the context for the application

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,52 @@
+import React, { createContext, useContext, useState } from "react";
+
+type User = {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+}
+
+type AuthContextData = {
+    user: User | null;
+    isSinginIng: boolean;
+    signIn: () => Promise<void>;
+    signOut: () => Promise<void>;
+}
+
+type AuthProviderProps = {
+    children: React.ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps){
+    const [isSinginIng, setIsSinginIng] = useState(false);
+    const [user, setUser] = useState<User | null>(null);
+
+    async function signIn(){
+
+    }
+
+    async function signOut(){
+        
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            signIn,
+            signOut,
+            user,
+            isSinginIng
+        }}>
+            { children}
+        </AuthContext.Provider>
+    )
+}
+
+function useAuth() {
+    const context = useContext(AuthContext);
+    return context;
+}
+
+export { AuthProvider, useAuth };
